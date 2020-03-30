8168e733ef455a8243827c761da5fb18ed33771a
Miriama Janosova <424615@mail.muni.cz>
2020-03-30 20:18:21 +0200

    main activity

--- app/app/src/main/java/com/example/receptarstarejmatere/MainActivity.kt
+++ app/app/src/main/java/com/example/receptarstarejmatere/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.receptarstarejmatere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_search.setOnClickListener{
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+        button_favorites.setOnClickListener{
+            val intent = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent)
+        }
+        button_recipes.setOnClickListener {
+            val intent = Intent(this, RecipesActivity::class.java)
+            startActivity(intent)
+        }
+        button_new_recipe.setOnClickListener {
+            val intent = Intent(this, NewRecipeActivity::class.java)
+            startActivity(intent)
+        }
+        button_settings.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/app/src/main/java/com/example/receptarstarejmatere/NewRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.receptarstarejmatere
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewRecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_recipe)
+    }
+}
