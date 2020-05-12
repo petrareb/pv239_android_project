baeaf21964aae8ac632af20d2a8feca140eed8bc
PetraR <peta.rebrosova@gmail.com>
2020-05-12 23:33:14 +0200

    make favorites clickable

--- app/app/src/main/java/com/example/receptarstarejmatere/activity/FavoritesActivity.kt
+++ app/app/src/main/java/com/example/receptarstarejmatere/activity/FavoritesActivity.kt
@@ -1,5 +1,6 @@
 package com.example.receptarstarejmatere.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -7,27 +8,39 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.receptarstarejmatere.R
 import com.example.receptarstarejmatere.adapter.FavoritesAdapter
 import com.example.receptarstarejmatere.application.App
+import com.example.receptarstarejmatere.database.model.Recipe
 import com.example.receptarstarejmatere.database.repository.RecipeRepository
+import com.example.receptarstarejmatere.utils.Constants
 
-class FavoritesActivity : AppCompatActivity() {
+class FavoritesActivity : AppCompatActivity(), FavoritesAdapter.OnFavoriteRecipeListener {
 
+    private var mFavorites: ArrayList<Recipe> = ArrayList()
     private lateinit var adapter: FavoritesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
 
-        adapter = FavoritesAdapter()
+        adapter = FavoritesAdapter(onFavoriteListener = this)
         initFavoritesRecyclerView(App.recipeRepository)
     }
 
     private fun initFavoritesRecyclerView(repo: RecipeRepository) {
         repo.getFavoriteRecipes().observe(this, Observer { recipes ->
+            mFavorites.clear()
+            mFavorites.addAll(recipes)
 
             adapter.swapData(recipes)
 
             val recyclerView = findViewById<RecyclerView>(R.id.favorites_list)
             recyclerView.adapter = adapter
         })
+    }
+
+    override fun onSelectedFavoriteRecipeClick(position: Int) {
+        val intent = Intent(this, RecipeOverviewActivity::class.java)
+        intent.putExtra(Constants.SELECTED_RECIPE_ID, mFavorites[position].id)
+        intent.putExtra(Constants.SELECTED_RECIPE_NAME, mFavorites[position].name)
+        startActivity(intent)
     }
 }
--- app/app/src/main/java/com/example/receptarstarejmatere/adapter/FavoritesAdapter.kt
+++ app/app/src/main/java/com/example/receptarstarejmatere/adapter/FavoritesAdapter.kt
@@ -9,11 +9,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.receptarstarejmatere.R
 import com.example.receptarstarejmatere.database.model.Recipe
 
-class FavoritesAdapter(private var recipes: List<Recipe> = listOf())
+class FavoritesAdapter(private var recipes: List<Recipe> = listOf(), private var onFavoriteListener: OnFavoriteRecipeListener)
     : RecyclerView.Adapter<FavoritesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.favorites_list_item, parent, false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.favorites_list_item,
+                parent,
+                false
+            ),
+            onFavoriteListener
+        )
     }
 
     override fun getItemCount(): Int {
@@ -29,13 +36,27 @@ class FavoritesAdapter(private var recipes: List<Recipe> = listOf())
         notifyDataSetChanged()
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var name: TextView = itemView.findViewById(R.id.favorites_name)
-        var star: ImageView = itemView.findViewById(R.id.favorites_star)
+    class ViewHolder(
+        private var favoriteView: View,
+        private var onFavoriteListener: OnFavoriteRecipeListener
+    ) : RecyclerView.ViewHolder(favoriteView), View.OnClickListener {
+
+        var name: TextView = favoriteView.findViewById(R.id.favorites_name)
+        var star: ImageView = favoriteView.findViewById(R.id.favorites_star)
 
         fun bind(recipe: Recipe) {
             name.text = recipe.name
             star.setImageResource(R.drawable.ic_star_yellow_24dp)
+
+            favoriteView.setOnClickListener(this)
         }
+
+        override fun onClick(v: View?) {
+            onFavoriteListener.onSelectedFavoriteRecipeClick(adapterPosition)
+        }
+    }
+
+    interface OnFavoriteRecipeListener {
+        fun onSelectedFavoriteRecipeClick(position: Int)
     }
 }
