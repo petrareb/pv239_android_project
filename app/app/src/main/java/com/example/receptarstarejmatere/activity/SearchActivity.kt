1788b3fc970a814ec7ddd78f98bd21479901e52a
PetraR <peta.rebrosova@gmail.com>
2020-05-23 23:45:29 +0200

    finish search

--- app/app/src/main/java/com/example/receptarstarejmatere/activity/SearchActivity.kt
+++ app/app/src/main/java/com/example/receptarstarejmatere/activity/SearchActivity.kt
@@ -1,26 +1,11 @@
 package com.example.receptarstarejmatere.activity
 
-import android.app.Activity
-import android.app.DownloadManager
-import android.app.SearchManager
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.SearchView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import com.example.receptarstarejmatere.R
-import com.example.receptarstarejmatere.adapter.RecipesAdapter
-import com.example.receptarstarejmatere.application.App
-import com.example.receptarstarejmatere.database.model.Recipe
 import com.example.receptarstarejmatere.utils.Constants
 import kotlinx.android.synthetic.main.activity_search.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class SearchActivity : AppCompatActivity() {
 
@@ -28,11 +13,15 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        button_search.setOnClickListener{
-            val intent = Intent(this, RecipesActivity::class.java)
+        button_search.setOnClickListener {
             val text = search_text.text.toString()
-            intent.putExtra(Constants.SEARCH_QUERY, text)
-            startActivity(intent)
+            if (text.isEmpty()) {
+                search_text.error = resources.getString(R.string.search_error_no_query)
+            } else {
+                val intent = Intent(this, RecipesActivity::class.java)
+                intent.putExtra(Constants.SEARCH_QUERY, text)
+                startActivity(intent)
+            }
         }
     }
 }
